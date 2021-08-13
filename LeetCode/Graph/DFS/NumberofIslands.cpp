bc57b1310396be9cb7806f0a33aee4e29656f3a5
fds-1 <62423463+fds-1@users.noreply.github.com>
2021-08-13 18:13:05 +0800

    add bfs

--- LeetCode/Graph/DFS/NumberofIslands.cpp
+++ LeetCode/Graph/DFS/NumberofIslands.cpp
@@ -40,3 +40,69 @@ public:
         else return;
     }
 };
+
+
+/* 
+ * Solution 2 bfs
+ * 2021.8.13 
+ */
+
+ /*
+  *  About     " auto i = near.front(); "    
+  *            " near.pop(); "
+  *             
+  *  in line 79, 80 :
+  *
+  *   Use " i " to get the point we check ,
+  *   to make sure the point we get is next point, delete the before one ,before add the next point to queue.
+  */
+
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+
+class Solution {
+public:
+    int num_islands = 0;
+    int numIslands(vector<vector<char>>& grid) {
+        // Storage the point and it`s place
+        queue<pair<int, int>> near;
+
+        for (int n = 0; n < grid.size(); ++n) {
+            for (int m = 0; m < grid[0].size(); ++m){
+                if(grid[n][m] == '1'){
+                    near.push({n, m});
+                    num_islands += 1;                    
+
+                    while(!near.empty()){
+                        auto i = near.front();
+                        near.pop();
+                        int nn = i.first;
+                        int mm = i.second;
+                        
+                        if(nn- 1>=  0              && grid[nn- 1][mm] == '1'){
+                            grid[nn- 1][mm] = '0';
+                            near.push({nn- 1, mm});
+                        }
+                        if(nn+ 1<  grid.size()     && grid[nn+ 1][mm] == '1'){
+                            grid[nn+ 1][mm] = '0';
+                            near.push({nn+ 1, mm});
+                        }
+                        if(mm- 1>= 0               && grid[nn][mm- 1] == '1'){
+                            grid[nn][mm- 1] = '0';
+                            near.push({nn, mm- 1});
+                        }
+                        if(mm+ 1<  grid[0].size()  && grid[nn][mm+ 1] == '1'){
+                            grid[nn][mm+ 1] = '0';
+                            near.push({nn, mm+ 1});
+                        }
+                    }
+                }
+            }
+        }
+    return num_islands;    
+    }
+};
+   
