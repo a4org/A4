edca7702450b2569b611e1397ea01ab6e3b0bea9
fds-1 <62423463+fds-1@users.noreply.github.com>
2021-08-15 20:40:00 +0800

    add bfs

--- LeetCode/Graph/DFS/SurroundedRegions.cpp
+++ LeetCode/Graph/DFS/SurroundedRegions.cpp
@@ -48,3 +48,80 @@ public:
         return;
     }
 };
+
+
+/*
+ * Solution 2: Bfs
+ * 2021.8.15
+ */
+
+ /* 
+  * In line 70 71 , use " r[i] " and " c[i] " to show four directions,
+  *
+  */
+ 
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+class Solution {
+public:
+    const int r[4] = {1, -1, 0, 0}; // Four directions
+    const int c[4] = {0, 0, 1, -1};
+    
+    void solve(vector<vector<char>>& board) {
+        int row = board.size();
+        int col = board[0].size();
+        queue<pair<int, int>> q;
+        
+      // Search " O " in four borders:
+        // Left and right
+        for(int i = 0; i< row; ++i){
+            if(board[i][0] == 'O'){
+                q.push({i, 0});
+                board[i][0] = 'T';
+            }
+            if(board[i][col- 1] == 'O'){
+                q.push({i, col- 1});
+                board[i][col- 1] = 'T';
+            }
+        }
+        
+        // Up and down
+        for(int i = 0; i< col; ++i){
+            if(board[0][i] == 'O'){
+                q.push({0, i});
+                board[0][i] = 'T';
+            }
+            if(board[row- 1][i] == 'O'){
+                q.push({row- 1, i});
+                board[row- 1][i] = 'T';
+            }
+        }
+        
+        // Bfs the " O " point, and replace it as " T " 
+        while(!q.empty()){
+            auto point = q.front();
+            q.pop();
+            int Row = point.first;
+            int Col = point.second;
+            
+            for (int i = 0; i < 4; i++) {
+                int R = Row + r[i];
+                int C = Col + c[i];
+                if (R< 0 || R>= row || C< 0 || C>= col || board[R][C] != 'O') continue;
+                board[R][C] = 'T';
+                q.emplace(R, C);
+            }  
+        }
+        
+        //
+        for(int i = 0; i< row; ++i){
+            for(int j = 0; j< col; ++j){
+                if     (board[i][j] == 'O') board[i][j] = 'X';
+                else if(board[i][j] == 'T') board[i][j] = 'O';
+            }
+        }
+    }
+};
