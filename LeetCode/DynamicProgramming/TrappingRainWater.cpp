faea2582150cec1f7906783232e224be27d9354b
fds-1 <62423463+fds-1@users.noreply.github.com>
2021-08-22 15:55:17 +0800

    2021.8.22 work Shuo Feng
    LeetCode 42 Trapping Rain Water

---
+++ LeetCode/DynamicProgramming/TrappingRainWater.cpp
@@ -0,0 +1,45 @@
+/*
+ * LeetCode 42 Trapping Rain Water
+ * Hard
+ * Shuo Feng
+ * 2021.8.22
+ */
+
+/*
+ * Solution :Dp.
+ *   For place " i ", find the biggest height in it`s left and right sides, get the shortest one between them (line 38)
+ *  if the shortest one higher than the height in place " i " ,the water that can be trapped in this place is ' shorest- height[i] ' (line 39).
+ *
+ *   About the the starting place when searching max height (line 30, 33)
+ *   height[] start with place " 0 " and end in place " size- 1 ", for one of them, point on it`s left or right is inexistence,
+ *  do not need to consider.
+ */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int sum = 0;
+        int size = height.size();
+        vector<int> height_L(size, 0);  // The biggest height to the left  of place " i ".
+        vector<int> height_R(size, 0);  // The biggest height to the right of place " i ".
+
+        for (int i = 1; i <= size - 1; ++i) 
+            height_L[i] = max(height_L[i - 1], height[i - 1]);  // Start from place " 1 ".
+        
+        for (int j = size - 2; j >= 0; --j) 
+            height_R[j] = max(height_R[j + 1], height[j + 1]);  // Start from place " size- 2 " 
+        
+
+        for (int i = 1; i < size - 1; ++i) {
+            int shortest = min(height_L[i], height_R[i]); // The most water that can be trapped.
+            if (shortest > height[i]) {
+                sum += shortest - height[i];
+            }
+        }
+        return sum;
+    }
+};
