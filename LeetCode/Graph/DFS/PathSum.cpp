d9a214033d6c28ae8fe51a404e8fb2b1b8d08929
Angold-4 <wangold4w@gmail.com>
2021-08-23 15:51:29 +0800

    2021 8.22 work (Fill)

--- LeetCode/Graph/DFS/PathSum.cpp
+++ LeetCode/Graph/DFS/PathSum.cpp
@@ -26,7 +26,7 @@ public:
         else 
             targetSum -= root->val;
        
-        if (targetSum == 0 && (root->left == nullptr && root->right ==nullptr))
+        if (targetSum == 0 && (root->left == nullptr && root->right == nullptr))
             return true;         
  
         return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);           
--- LeetCode/Graph/DFS/PathSumii.cpp
+++ LeetCode/Graph/DFS/PathSumii.cpp
@@ -1,9 +1,74 @@
 /* 
  * LeetCode 113 Path Sum ii
- * Medium
+ * Easy
  * Jiawei Wang
- * 2021.8.21
+ * 2021.8.23
  */
 
+#include <vector>
+#include <deque>
+
+using namespace::std;
+
+ struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
 
 
+deque<int> current;
+vector<int> tmp;
+vector<vector<int>> ans;
+
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+	dfs(root, targetSum);
+	return ans;
+    }
+
+    void dfs(TreeNode* root, int targetSum) {
+	if (root == nullptr) {
+	    return;
+	} else {
+	    targetSum -= root->val;
+	}
+
+
+	current.push_back(root->val);
+
+	dfs(root->left, targetSum);
+	dfs(root->right, targetSum);
+
+	if (root->right == nullptr && root->left == nullptr && targetSum == 0)  {
+	    ans.push_back({});
+	    for (auto s : current) {
+		ans.back().push_back(s);
+	    }
+	}
+	current.pop_back();
+    }
+
+    void dfs2(TreeNode* root, int targetSum) {
+	// using vector
+	if (root == nullptr) {
+	    return;
+	} else {
+	    targetSum -= root->val;
+	}
+
+	tmp.push_back(root->val);
+
+	dfs(root->left, targetSum);
+	dfs(root->right, targetSum);
+
+	if (root->right == nullptr && root->left == nullptr && targetSum == 0)  {
+	    ans.push_back(tmp);
+	}
+	tmp.pop_back();
+    }
+};
