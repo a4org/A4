1ff9591fad9d175d35f4d3a7240c1bdb63b226e0
Angold-4 <wangold4w@gmail.com>
2021-09-02 19:53:52 +0800

    2021.9.2 Revision Jiawei Wang

--- LeetCode/Tree/BinaryTree/BinaryTreePreorderTraversal.cpp
+++ LeetCode/Tree/BinaryTree/BinaryTreePreorderTraversal.cpp
@@ -10,6 +10,11 @@
  * [root, left, right]
  */
 
+/* Revision
+ * $1 2021.9.2 Jiawei Wang
+ * Subtree
+ */
+
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -30,14 +35,16 @@ class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
 	vector<int> results;
-	stack<TreeNode*> Stack;
+	stack<TreeNode*> Stack; // Stack stores all nodes(root) with its unvisited right subtrees
 
 	while (root != NULL || !Stack.empty()) {
 	    if (root != NULL) {
+		// we can go further left
 		Stack.push(root);
 		results.push_back(root->val);
 		root = root->left;
 	    } else {
+		// when hit leaf, means we cannot go left further in this subtree
 		root = Stack.top();
 		Stack.pop();
 		root = root->right;
