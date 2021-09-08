352322119e9122cda9c01731e8faa95ed565a1c2
fds-1 <62423463+fds-1@users.noreply.github.com>
2021-09-08 16:54:04 +0800

    2021.9.8 revision Shuo Feng
    LeetCode 98 Validate Binary Search Tree

--- LeetCode/Tree/BinaryTree/ValidateBinarySearchTree.cpp
+++ LeetCode/Tree/BinaryTree/ValidateBinarySearchTree.cpp
@@ -3,10 +3,11 @@
  * Medium
  * Shuo Feng
  * 2021.8.9
+ * Last edited at 2021.9.8
  */
 
 /*
- *   Inorder traverse the bnary tree and record in 'list',if it is a valid binary search tree 
+ *   Inorder traverse the binary tree and record value of each nodes in 'list', if it is a valid binary search tree 
  *  the values in 'list' should be sorted from smallest to largest .
  */
 
@@ -16,34 +17,34 @@ using namespace std;
 
  //Definition for a binary tree node.
  struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
- 
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
     vector<int> list;
     bool isValidBST(TreeNode* root) {
-        //if(!root) return true;
-        //if(root-> left == nullptr && root-> right == nullptr) return true;
-
         traverse(root);
-        for(int i = 1; i< list.size(); ++i){
-                if(list[i]<= list[i- 1]) return false;
+     
+        // Check whether "list" is a increasing sequence.
+        for (int i = 1; i < list.size(); ++i) {
+            if (list[i] <= list[i - 1]) return false;  // Binary tree is invalid
         }
-    return true;    
+        return true;
     }
-    void traverse(TreeNode* root){
-        if(root){
-            traverse(root-> left );   //Inorder traversal
-            list.push_back(root-> val);
-            traverse(root-> right);
+ 
+    // Inorder traversal.
+    void traverse(TreeNode* root) {
+        if (root) {
+            traverse(root->left);
+            list.push_back(root->val);
+            traverse(root->right);
         }
         else return;
     }
-
 };
