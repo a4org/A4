d4159aa9c0d378e48f23e545b7dddf0ef5fbf85b
Shuo Feng <62423463+fds-1@users.noreply.github.com>
2021-09-22 23:40:45 +0800

    2021.9.22 Revision Shuo Feng
    LeetCode 120 Triangle

--- LeetCode/DynamicProgramming/Triangle.cpp
+++ LeetCode/DynamicProgramming/Triangle.cpp
@@ -3,37 +3,39 @@
  * Medium
  * Shuo Feng
  * 2021.8.23
+ * Last edited at 2021.9.22
  */
-
-/*
- * Solution 1: Dp (from top to bottom)
- *
- *   Creat a double dimensional array sum, each place storage the shortest path to it.
- *   There are two paths lead to place" [i][j] "(besides left and right boundary, [0][0]), and the sum path is " sum[i - 1][j - 1] + triangle[i][j] "
- *  or " sum[i - 1][j] + triangle[i][j] ", choose the shortest one.
- */ 
 
 #include<iostream>
 #include<vector>
 using namespace std;
 
+/*
+ * Solution 1: Dp (from top to bottom)
+ *
+ *   Creat a double dimensional array 'sum', for each place storage the shortest path to it.
+ *   There are two paths lead to place" [i][j] "(besides left and right boundary, [0][0]), and the path sum is " sum[i - 1][j - 1] + triangle[i][j] "
+ *  or " sum[i - 1][j] + triangle[i][j] ", choose the shortest one.
+ */  
+
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>> sum(size, vector<int>(size));
+        vector<vector<int>> sum(size, vector<int>(size)); // Used to storage the shortest way.
         int size = triangle.size();
         sum[0][0] = triangle[0][0];
 
         for (int i = 1; i < size; ++i) {
-            sum[i][0] = sum[i - 1][0] + triangle[i][0]; // The left boundary
+            sum[i][0] = sum[i - 1][0] + triangle[i][0]; // The left boundary.
             int size_2 = triangle[i].size();
-
+ 
             for (int j = 1; j < size_2; ++j) {
-                sum[i][j] = min(sum[i - 1][j - 1], sum[i - 1][j]) + triangle[i][j];
+                sum[i][j] = min(sum[i - 1][j - 1], sum[i - 1][j]) + triangle[i][j]; // Places between two boundaries
             }
             sum[i][i] = sum[i - 1][i - 1] + triangle[i][i]; // The right boundary
         }
 
+        // Select the minimum one in last line.
         int Min = INT_MAX;
         for (int i = 0; i < size; ++i) {
             if (sum[size - 1][i] < Min)
@@ -48,8 +50,8 @@ public:
  * Solution 2: Dp (from bottom to top)
  * The better way
  * 
- *   Begin with the line before the last line, for each place in this line there are two alternative paths, choose the minimum one
- *  and add it. In next cycle, move up and repeat, when reach the last place( [0][0] ), it`s number is the minimum path.
+ *   Begin with the line before the last line, for place 'j' in this line there are two optional paths to go, choose the minimum one
+ *  and add it to place 'j'. In next cycle, move up and repeat, when reach the last place( [0][0] ), it`s number is the the minimum path.
  */
 
 #include<iostream>
@@ -64,6 +66,7 @@ public:
             int size_2 = triangle[i].size();
 
             for (int j = 0; j < size_2; ++j) {
+                // Choose the minimum one beetween the next two paths.
                 triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1]);
             }
         }
