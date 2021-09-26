d1ecb659126455714f895a3722ff7470ca3c7648
Shuo Feng <62423463+fds-1@users.noreply.github.com>
2021-09-26 22:25:35 +0800

    2021.9.26 Revision Shuo Feng
    LeetCode 139 Work Break

--- LeetCode/DynamicProgramming/WordBreak_f.cpp
+++ LeetCode/DynamicProgramming/WordBreak_f.cpp
@@ -3,6 +3,7 @@
  * Medium
  * Shuo Feng
  * 2021.8.27 
+ * Last edited at 2021.9.26
  */
 
 /*
@@ -34,17 +35,22 @@ public:
             maxlen = max(maxlen, size_2);
             minlen = min(minlen, size_2);
         }
+        
+        // Search begin with minimum size.
         // From starting point, search the interval before it.
         for (int i = minlen; i <= size; ++i) {
             
-          // Space before i should be bigger than the maximum length, otherwise start with " 0 ".
+            // Space before i should be bigger than the maximum length, otherwise start with " 0 ".
             for (int j = max(i - maxlen, 0); j < i; ++j) {
+                
+                // First j letters can be segmented and 'i'size substr can be found in table.
                 if (dp[j] && mem.find(s.substr(j, i - j)) != mem.end()) {
                     dp[i] = 1;
                     break;
                 }
             }
         }
+        // Whether the first 'size' letters can be segmented( whole string).
         return dp[size];
     }
 
