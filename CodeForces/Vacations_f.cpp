5748d3f9142e4e24df6d0e6f64295c9dd81c87a6
Shuo Feng <62423463+fds-1@users.noreply.github.com>
2021-09-30 00:16:48 +0800

    2021.9.30 Revision Shuo Feng
    CodeForces 698A Vacations

--- CodeForces/Vacations_f.cpp
+++ CodeForces/Vacations_f.cpp
@@ -2,6 +2,7 @@
  * CodeForces 698A Vacations
  * Shuo Feng
  * 2021.8.30
+ * Last edited at 2021.9.30
  */
 
 /*
@@ -25,18 +26,18 @@ int main() {
         cin >> situ[i];
     }
     dp[0] = 0;
-    int situ_before = 0; // Situation before the day.
+    int situ_before = 0; // Situation before the day, default rest before vacation.
 
     for (int i = 1; i <= days; ++i) {
         
-        // (1). The gym is closed and the contest is not carried out --> can only rest.
+        // ◆(1). The gym is closed and the contest is not carried out --> can only rest.
         if (situ[i] == 0) {
-            dp[i] = dp[i - 1] + 1;
-            situ_before = 0;
+            dp[i] = dp[i - 1] + 1; // On the basic of day 'i - 1', add rest day.
+            situ_before = 0; // Upadte situation_before.
         }
         else if (situ[i] == 1 || situ[i] == 2) {
             
-            // (2). Only gym is open or only carried the contest, two cases:  1. do the same activity --> rest.
+            // ◆(2). Only gym is open or only carried the contest, two cases:  1. do the same activity --> rest.
             //                                                                2. else                 --> activity.
             if (situ[i] == situ_before) {
                 dp[i] = dp[i - 1] + 1;
@@ -48,11 +49,11 @@ int main() {
             }
         }
         
-        // (3). The gym is open and the contest is carried out --> can always find something to do.
-        //      Judge of the situation:
+        // ◆(3). The gym is open and the contest is carried out --> can always find something to do.
+        //      Judge of the situation (explame about line 59 'situ_before = situ[i] - situ_before;'):
         //         1. If situation before is " 2 ", then write the contest (situation 1 ).
         //         2. If situation before is " 1 ", then do sport          (situation 2 ).
-        //         3. If situation before is " 0 ", write the contest, do sport, rest are possible (situation 3 ).( About line: 58)
+        //         3. If situation before is " 0 ", write the contest, do sport, rest are possible (situation 3 ).
         else if (situ[i] == 3) {
             dp[i] = dp[i - 1];
             situ_before = situ[i] - situ_before;
