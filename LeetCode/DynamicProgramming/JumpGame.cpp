4959b7e9b62473e12e65d13ef366c2d1a4d66096
Angold-4 <wangold4w@gmail.com>
2021-10-03 17:40:20 +0800

    2021.10.3 Oct Challenge #3 Jiawei Wang

--- LeetCode/DynamicProgramming/JumpGame.cpp
+++ LeetCode/DynamicProgramming/JumpGame.cpp
@@ -5,6 +5,10 @@
  * 2021 8.11
  */
 
+/* Revision
+ * $1 2021.10.3 Jiawei Wang
+ */
+
 #include <vector>
 
 using namespace::std;
@@ -21,5 +25,24 @@ public:
 	    if (maxjump >= size-1) return true;
 	}
 	return false;
+    }
+
+    // dp[i] => the maximum jump distance in index i
+    bool canJump2(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> dp(n);
+     
+    
+        if (n == 1 || n == 0) return true;
+        if (nums[0] == 0) return false;
+        dp[0] = nums[0];
+
+        for (int i = 1; i < n-1; i++) {
+            dp[i] = max(dp[i-1]-1, nums[i]);
+            if (dp[i] == 0) return false; 
+        }
+        
+        return (dp[n-2] > 0);
     }
 };
