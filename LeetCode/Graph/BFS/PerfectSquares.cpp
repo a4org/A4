fe565fe5a836fec947607c4af60b2549d45c804f
Shuo Feng <62423463+fds-1@users.noreply.github.com>
2021-10-14 21:16:12 +0800

    2021.10.14 Update Shuo Feng
    LeetCode 279 Perfect Squares

--- LeetCode/Graph/BFS/PerfectSquares.cpp
+++ LeetCode/Graph/BFS/PerfectSquares.cpp
@@ -3,19 +3,20 @@
  * Medium
  * Shuo Feng 
  * 2021.8.16
- * Last edited at 2021.9.15
+ * Last edited at 2021.9.15 / 2021.10.14
  */
 
 /*
  * Solution 1: Bfs.
- *   Begin with 0, in each cycles, add the numbers which`s inconformity to the queue.
+ *   Begin with 0, in each cycles, add the numbers which`s inconformity with n to the queue.
  *   When the number is consistent with n ,return the result.
+ *
  *   For example:  
  *  n = 8:     0
- *            /  \
- *           1     4      
+ *            /  \    \
+ *           1     4    9 (Exclude: "0 + 9" bigger than n).   
  *          /  \  /  \
-           1   4  1   4 ←─ The first number that can form " n " with number before, there is the min_nums: 2
+ *         1   4  1   4 ←─ The first number that can form " n " with numbers before, there is the least number: 2
  */
 
 #include<iostream>
@@ -28,6 +29,8 @@ public:
     int numSquares(int n) {
         int min_nums = 0;
         queue<int> q;
+               
+        // Start with 0.
         q.push(0);
 
         while(!q.empty()){
@@ -42,8 +45,7 @@ public:
                 for(int j = 0; (j* j)+ m <= n; ++j){
                     int num =(j* j)+ m;
                     if(num == n) return min_nums;
-                    else if(num < n) q.push(num);
-                    else break;
+                    else q.push(num);
                 }
             }
         }
