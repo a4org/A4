72f246a6b4790fd5e8904b9a10eaacafbaee8d02
Angold-4 <wangold4w@gmail.com>
2021-10-19 16:07:31 +0800

    2021.10.19 Oct Challenge #19 Monotonic Stack Jiawei Wang

---
+++ LeetCode/Stack/MonotonicStack/NextGreaterElementi.cpp
@@ -0,0 +1,50 @@
+/*
+ * LeetCode 496 Next Greater Element
+ * Easy
+ * Jiawei Wang
+ * 2021.10.19
+ */
+
+#include <vector>
+#include <unordered_map>
+#include <stack>
+
+using namespace::std;
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+	unordered_map<int, int> Map;
+
+	stack<int> Stack;
+	// Monotonic Stack: Keep the Stack increasing / decreasing
+
+	for (int x : nums2) {
+	    if (Stack.empty()) Stack.push(x);
+	    else if (x <= Stack.top()) {
+		// Keep the Stack decreasing
+		Stack.push(x);
+	    }
+
+	    else {
+		// 6 5 3 2 1 4
+		// 6 5       4
+		while (!Stack.empty() && Stack.top() < x) {
+		    Map[Stack.top()] = x;
+		    Stack.pop();
+		}
+		Stack.push(x);
+	    }
+
+	}
+
+	vector<int> ret;
+	for (auto y : nums1) {
+	    if (Map.find(y) == Map.end()) ret.push_back(-1);
+	    else ret.push_back(Map[y]);
+	}
+
+	return ret;
+    }
+
+};
