d4b3fb441f1fb9d7d0d5ab14f5b44ef5bd3cde89
Angold-4 <wangold4w@gmail.com>
2021-11-03 20:17:57 +0800

    we do not need curr to store each value

--- LeetCode/Graph/DFS/SumRootToLeafNumbers.cpp
+++ LeetCode/Graph/DFS/SumRootToLeafNumbers.cpp
@@ -19,37 +19,34 @@ public:
     int sumNumbers(TreeNode* root) {
         // DFS + BackTracking
         int ret = 0;
-        vector<int> curr, currSum;
+        vector<int> currSum;
 	currSum.push_back(0);
-        helper(root, ret, curr, currSum);
+        helper(root, ret, currSum);
         return ret;
     }
 private:
-    void helper(TreeNode* root, int& ret, vector<int>& curr, vector<int>& currSum) {
+    void helper(TreeNode* root, int& ret, vector<int>& currSum) {
         if (root == nullptr) {
             // Termination Condtion #1
             return;
         }
         
-        curr.push_back(root->val);
         currSum.push_back(currSum.back()*10 + root->val);
         if (root->left == nullptr && root->right == nullptr) {
             // got one answer
             ret += currSum.back();
-            curr.pop_back();
             currSum.pop_back();
             return;
         } 
         
         if (root->left != nullptr) {
-            helper(root->left, ret, curr, currSum);
+            helper(root->left, ret, currSum);
         }
         
         if (root->right != nullptr) {
-            helper(root->right, ret, curr, currSum);
+            helper(root->right, ret, currSum);
         }
         
-        curr.pop_back();
         currSum.pop_back();
         return; 
     }
