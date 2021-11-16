919ad49f91c5751d5baf5037d8056e6f89a175d3
Angold-4 <wangold4w@gmail.com>
2021-11-17 00:39:42 +0800

    Understand this 668

--- LeetCode/Array/BinarySearch/KthSmallestNumberInMultiplicationTable.cpp
+++ LeetCode/Array/BinarySearch/KthSmallestNumberInMultiplicationTable.cpp
@@ -4,5 +4,35 @@
  * Jiawei Wang
  * 2021.11.16
  */
+#include <vector>
 
+using namespace::std;
 
+class Solution {
+public:
+    int findKthNumber(int m, int n, int k) {
+	int left = 1;
+	int right = m*n;
+
+	while (left < right) {
+	    int val = left - (right-left)/2;
+
+	    if (countEqualOrSmaller(val, m, n) < k) {
+		left = val + 1;
+	    } else {
+		right = val;
+	    }
+	}
+	return left;
+
+    }
+private:
+    int countEqualOrSmaller(int val, int m, int n) {
+	// check how many numbers that 
+	int count = 0;
+	for (int i = 1; i <= m; i++) {
+	    count += min(n, val/i); // every lines
+	}
+	return count;
+    }
+};
