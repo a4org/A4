fa1493fe07dc281f289e58557e230de63b340d56
Angold-4 <wangold4w@gmail.com>
2022-01-04 01:27:16 +0800

    2022.1.03 Work Youxiu #1 Jiawei Wang Greedy LIS

--- LeetCode/DynamicProgramming/LongestIncreasingSubsequence_w.cpp
+++ LeetCode/DynamicProgramming/LongestIncreasingSubsequence_w.cpp
@@ -5,25 +5,53 @@
  * 2021 9.8
  */
 
+/* Revision
+ * $1 2022.1.3 Jiawei Wang
+ */
+
 #include <vector>
 
 using namespace::std;
 
 class Solution {
 public:
+    // #1 DP
+    // DP Basic II
+    // O(n^2)
     int lengthOfLIS(vector<int>& nums) {
 	int n = nums.size();
-	int ret = 1;
-	vector<int> dp(n, 1);
-	// dp[i] := the num of numbers in nums[i, n] that are strictly increasing
-	for (int i = n-1; i >= 0; i--) {
-	    for (int j = i; j < n; j++) {
-		if (nums[j] > nums[i]) {
-		    dp[i] = max(dp[j]+1, dp[i]);
-		    ret = max(dp[i], ret);
+	vector<int> dp(n, 1); 
+	// dp[i]: the length of longest increasing subsequence for nums[0:i]
+	// ending with nums[i]
+
+	for (int i = 0; i < n; i++) {
+	    for (int j = 0; j < i; j++) {
+		if (nums[j] < nums[i]) {
+		    dp[i] = max(dp[i], dp[j]+1);
 		}
 	    }
 	}
+	int ret = 0;
+	for (auto n : dp) ret = max(ret, n);
 	return ret;
+    }
+
+    // #2 Greedy
+    // O(nlogn)
+    // https://www.youtube.com/watch?v=Q6KyDl_xiIg
+    int lengthOfLIS2(vector<int>& nums) {
+	int n = nums.size();
+	vector<int> q; // longest Increasing Subsequence (LIS)
+
+	for (int x : nums) {
+	    auto pos = lower_bound(q.begin(), q.end(), x); // Binary search
+	    // find the first position that greater than x in q
+	    if (pos == q.end()) {
+		q.push_back(x);  // just push_back
+	    } else {
+		*pos = x; // make a "better" LIS
+	    }
+	}
+	return q.size();
     }
 };
