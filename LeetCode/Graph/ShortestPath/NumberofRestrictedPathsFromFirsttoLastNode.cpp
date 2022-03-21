add0dc1b5a67f01eb2cc09009f9f1425a89b9e08
Angold-4 <wangold4w@gmail.com>
2022-03-22 02:35:58 +0800

    2022.3.21 Cruel Coding Jiawei Wang

---
+++ LeetCode/Graph/ShortestPath/NumberofRestrictedPathsFromFirsttoLastNode.cpp
@@ -0,0 +1,25 @@
+/* 
+ * LeetCode 1786 Number of Restricted Paths From First to Last Node
+ * Medium
+ * Jiawei Wang
+ * 2022.3.21
+ */
+
+#include <vector>
+#include <queue>
+
+using namespace::std;
+
+using LL = long long;
+using PLL = pair<LL, LL>;
+
+class Solution {
+public:
+    int countRestrictedPaths(int n, vector<vector<int>>& edges) {
+	int ret = 0;
+
+	// 1. find the shortest path
+        
+    }
+};
+
