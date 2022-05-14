4e11a0f6ecd987ed65b1955375968fd493ba0972
Angold-4 <wangold4w@gmail.com>
2022-05-14 19:26:42 +0800

    2022.5.14 LeetCode Diff array

---
+++ LeetCode/Array/CarPooling.cpp
@@ -0,0 +1,34 @@
+/*
+ * LeetCode 1094 Car Pooling
+ * Medium
+ * Jiawei Wang
+ * 2022.5.14
+ */
+
+#include <vector>
+#include <algorithm>
+
+using namespace::std;
+
+class Solution {
+public:
+  // diff array
+  bool carPooling(vector<vector<int>>& trips, int capacity) {
+    vector<pair<int, int>> diff;
+    for (auto trip : trips) {
+      diff.push_back({trip[1], trip[0]});
+      diff.push_back({trip[2], -trip[0]});
+    }
+
+    sort(diff.begin(), diff.end()); 
+    // sorting on pair will consider each element
+
+    int num = 0;
+    for (auto d : diff) {
+      num += d.second;
+      if (num > capacity) return false;
+    }
+
+    return true;
+  }
+};
