97a35444f718d8fd432e7f1bb52fa75d485e47e0
Angold-4 <wangold4w@gmail.com>
2022-05-19 13:04:09 +0800

    2022.5.18 diff array Cruel coding

---
+++ LeetCode/Array/MaximumSumObtainedofAnyPermutation.cpp
@@ -0,0 +1,42 @@
+/*
+ * LeetCode 1589 Maximum Sum Obtained of Any Permutation
+ * Medium
+ * Jiawei Wang
+ * 2022.5.19
+ */
+
+#include <vector>
+#include <algorithm>
+
+using namespace::std;
+
+class Solution {
+public:
+  int mod = 1000000007;
+  int maxSumRangeQuery(vector<int>& nums, vector<vector<int>>& requests) {
+    // Diff array
+    int n = nums.size();
+    vector<long> diff(n+2, 0);
+
+    for (auto request : requests) {
+      diff[request[0]]++;
+      diff[request[1]+1]--;
+    }
+
+    for (int i = 1; i < n; i++) {
+      diff[i] += diff[i-1];
+    }
+
+    sort(diff.rbegin(), diff.rend());
+    sort(nums.rbegin(), nums.rend());
+
+    long ret = 0;
+    for (int i = 0; i < n; i++) {
+      int over = diff[i] * nums[i];
+      over %= mod;
+      ret += over;
+    }
+
+    return ret % mod;
+  }
+};
