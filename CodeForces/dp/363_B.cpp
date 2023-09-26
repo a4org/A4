b9f003db65d88cd907c8451e9f46e5963f6e194f
Angold-4 <wangold4w@gmail.com>
2023-09-26 16:25:35 +0800

    DP 363 B

---
+++ CodeForces/dp/363_B.cpp
@@ -0,0 +1,108 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <sstream>
+#include <set>
+#include <vector>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+// https://codeforces.com/blog/entry/76087
+template<class T1, class T2>
+std::ostream &operator<<(std::ostream &os, const std::pair<T1,T2> &p){
+  os << '{' << p.first << ", " << p.second << '}';
+  return os;
+}
+
+template<typename C, 
+         typename T = std::decay<decltype(*begin(std::declval<C>()))>,
+         typename std::enable_if<!std::is_same<C, std::string>::value>::type* = nullptr
+         >
+std::ostream &operator<<(std::ostream &os, const C &container){
+  bool first = true;
+  std::stringstream ss; 
+  ss << '[';
+  for(const auto &x : container){
+    if (!first){
+      ss << ", ";
+    }
+    first = false;
+    ss << x;
+  }
+  ss << ']';
+  return os << ss.str();
+}
+
+vector<string> vec_splitter(string s) {
+	s += ',';
+	vector<string> res;
+	while(!s.empty()) {
+		res.push_back(s.substr(0, s.find(',')));
+		s = s.substr(s.find(',') + 1);
+	}
+	return res;
+}
+
+void debug_out(
+vector<string> __attribute__ ((unused)) args,
+__attribute__ ((unused)) int idx, 
+__attribute__ ((unused)) int LINE_NUM) { cerr << endl; } 
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+	if(idx > 0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+	stringstream ss; ss << H;
+	cerr << args[idx] << " = " << ss.str();
+	debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#ifdef DEBUG
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+int helper(int* fence, int i, int k, int prev) {
+  prev -= fence[i-1]; // starting from i;
+  prev += fence[i+k-1];
+  return prev;
+}
+
+
+int fence[150000]; // better using array in the heap
+int main() {
+  int n, k, tmp; cin >> n >> k;
+  for (int i = 0; i < n; i++) {
+    cin >> tmp; 
+    fence[i] = tmp;
+  }
+
+  int sum = 0;
+  int ans = 0;
+  for (int i = 0; i < k; i++) {
+    sum += fence[i];
+  }
+
+  tmp = sum;
+  for (int i = 1; i <= n-k; i++) {
+    tmp = helper(fence, i, k, tmp);
+    if (tmp < sum) {
+      sum = tmp;
+      ans = i;
+    }
+  }
+
+  cout << ans+1 << endl;
+}
