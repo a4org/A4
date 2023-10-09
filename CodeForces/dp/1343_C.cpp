10df5e849795a1eab9c3efbe228c9b859249ec56
Angold-4 <wangold4w@gmail.com>
2023-10-10 03:19:05 +0800

    Not a DP problem, sometimes dp will narrowing your mind

---
+++ CodeForces/dp/1343_C.cpp
@@ -0,0 +1,119 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <sstream>
+#include <set>
+#include <vector>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+// https://codeforces.com/blog/entry/76087
+template<class T1, class T2>
+std::ostream &operator<<(std::ostream &os, const std::pair<T1,T2> &p){
+  os << '{' << p.first << ", " << p.second << '}';
+  return os;
+}
+
+template<typename C, 
+         typename T = std::decay<decltype(*begin(std::declval<C>()))>,
+         typename std::enable_if<!std::is_same<C, std::string>::value>::type* = nullptr
+         >
+std::ostream &operator<<(std::ostream &os, const C &container){
+  bool first = true;
+  std::stringstream ss; 
+  ss << '[';
+  for(const auto &x : container){
+    if (!first){
+      ss << ", ";
+    }
+    first = false;
+    ss << x;
+  }
+  ss << ']';
+  return os << ss.str();
+}
+
+vector<string> vec_splitter(string s) {
+	s += ',';
+	vector<string> res;
+	while(!s.empty()) {
+		res.push_back(s.substr(0, s.find(',')));
+		s = s.substr(s.find(',') + 1);
+	}
+	return res;
+}
+
+void debug_out(
+vector<string> __attribute__ ((unused)) args,
+__attribute__ ((unused)) int idx, 
+__attribute__ ((unused)) int LINE_NUM) { cerr << endl; } 
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+	if(idx > 0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+	stringstream ss; ss << H;
+	cerr << args[idx] << " = " << ss.str();
+	debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#ifdef DEBUG
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+void run_case() {
+  int n; cin >> n;
+
+  vector<long long> a(n);
+  for (auto &x : a) cin >> x;
+
+
+  long long maxi = a[0];
+  bool pos = maxi > 0;
+  long long ret = 0;
+
+  if (n == 1) ret = maxi;
+  else {
+    // group the number with same sign together (greedy)
+    for (int i = 1; i < n; i++) {
+      long long tmp = a[i];
+      if ((pos && tmp > 0) || (!pos && tmp < 0)) {
+        // sign unchanged
+        maxi = max(maxi, tmp);
+        if (i == n-1) {
+          ret += maxi; 
+        }
+      } else {
+        ret += maxi;
+        maxi = tmp;
+        pos = !pos;
+        if (i == n-1) {
+          ret += tmp;
+        }
+      }
+    }
+  }
+
+  cout << ret << '\n';
+}
+
+int main() {
+  int tests;
+  cin >> tests;
+
+  while (tests-- > 0) 
+    run_case();
+}
